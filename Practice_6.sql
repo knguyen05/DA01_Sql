5adc7bb2c53ede1f367bc885461104f370cf2a8f
knguyen05 <153193093+knguyen05@users.noreply.github.com>
2023-12-17 23:02:53 +0700

    Update Practice_6.sql

--- Practice_6.sql
+++ Practice_6.sql
@@ -82,10 +82,10 @@ on table1.month=table2.month and table1.country=table2.country;
 SELECT TO_CHAR(trans_date, 'yyyy-mm') as month, country,
 COUNT(*) as trans_count,
 SUM(CASE 
-WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+WHEN state = 'approved' THEN 1 ELSE 0 END) as approved_count,
 SUM(amount) as trans_total_amount,
 SUM(CASE 
-WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
 FROM Transactions
 GROUP BY month, country;
 
