fb40d11c7b92b420d87a88f7e9518fbe25e4dd79
knguyen05 <153193093+knguyen05@users.noreply.github.com>
2023-12-26 02:57:15 +0700

    Update Project_1.sql

--- Project_1.sql
+++ Project_1.sql
@@ -30,3 +30,17 @@ SET QTR_ID = CASE WHEN 1<=MONTH_ID and MONTH_ID<=3 THEN 1
 					WHEN 7<=MONTH_ID and MONTH_ID<=9 THEN 3 ELSE 4 END;
 
 --5 Hãy tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách) ( Không chạy câu lệnh trước khi bài được review)
+--cach 1_box_plot
+with outlier as
+(select Q1-1.5*IQR as min_value, Q3+1.5*IQR as max_value
+from(
+select
+percentile_cont(0.25) WITHIN GROUP (ORDER BY QUANTITYORDERED ) as Q1,
+percentile_cont(0.75) WITHIN GROUP (ORDER BY QUANTITYORDERED ) as Q3,
+percentile_cont(0.75) WITHIN GROUP (ORDER BY QUANTITYORDERED )-percentile_cont(0.25) WITHIN GROUP (ORDER BY QUANTITYORDERED ) as IQR
+from sales_dataset_rfm_prj) a ) 
+Select * from outlier
+where users< (select min_value from twt_min_max_value)
+or users>(select max_value from twt_min_max_value)
+
+--cach2_Z-score
